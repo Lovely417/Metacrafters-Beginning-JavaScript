da15f577a98342625aed6c73b4d2f793fa76f34c
Lovely Sharma <lovelysharma43276@gmail.com>
2024-05-23 20:41:45 +0530

    second commit

--- nftFun.js
+++ nftFun.js
@@ -9,17 +9,29 @@ Assessment Requirements
 */
 
 // create a variable to hold your NFT's
+const NFTs = []
 
 // this function will take in some values as parameters, create an
 // NFT object using the parameters passed to it for its metadata, 
 // and store it in the variable above.
-function mintNFT () {
+function mintNFT (name, eyeColor, shirtType, bling) {
+    const myNFT = {
+        "Name": name,
+        "EyeColor": eyeColor,
+        "ShirtType": shirtType,
+        "Bling": bling
+    }
+    NFTs.push(myNFT);
+    console.log("Minted: "+ name);
 
 }
 
 // create a "loop" that will go through an "array" of NFT's
 // and print their metadata with console.log()
 function listNFTs () {
+    for( let i = 0; i< NFTs.length; i++){
+        console.log(NFTs[i]);
+    }
 
 }
 
